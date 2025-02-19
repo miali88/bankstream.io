9f64d99f59261611ce2d4d0227c1922f53279241
miali88 <m.ali@gacm.co.uk>
2025-02-19 16:59:38 +0000

    ngrok

--- ngrok.py
+++ ngrok.py
@@ -9,18 +9,18 @@ logging.info('Starting ngrok tunnel setup')
 try:
 
     """ static domain - Frontend (Vite)"""
-    port = 8001
+    port = 5190
     http_tunnel_static = ngrok.connect(addr=f"http://localhost:{port}", subdomain="internally-wise-spaniel")
     logging.info(f"Frontend (Vite) running on:\n"
                 f"  - Local: http://localhost:{port}\n"
                 f"  - Ngrok: {http_tunnel_static.public_url}")
     
     """ dynamic domain - Backend (FastAPI)"""   
-    # port = 8001
-    # http_tunnel_dynamic = ngrok.connect(addr=f"http://localhost:{port}")
-    # logging.info(f"Backend (FastAPI) running on:\n"
-    #             f"  - Local: http://localhost:{port}\n"
-    #             f"  - Ngrok: {http_tunnel_dynamic.public_url}")
+    port = 8001
+    http_tunnel_dynamic = ngrok.connect(addr=f"http://localhost:{port}")
+    logging.info(f"Backend (FastAPI) running on:\n"
+                f"  - Local: http://localhost:{port}\n"
+                f"  - Ngrok: {http_tunnel_dynamic.public_url}")
 
     # """ dynamic domain"""   
     # port = 5185
