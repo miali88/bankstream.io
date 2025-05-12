1998eaf0067c656d201d69ba6e460241285f4e04
miali88 <m.ali@gacm.co.uk>
2025-05-12 21:36:34 +0100

    package updates

--- frontend/app/routes/sign-in.$.tsx
+++ frontend/app/routes/sign-in.$.tsx
@@ -21,7 +21,6 @@ export default function SignInPage() {
       <div className="absolute top-4 left-4">
         {/* Logo can be added here */}
       </div>
-      <p>HE HAHHH</p>
       <div className="flex justify-center items-center min-h-screen">
         <SignIn routing="path" path="/sign-in" signUpUrl="/sign-up" />
       </div>
